7a311d7281636fe4c02134bd7cd9f92e0bd690d9
sprout90 <mikebrehm420@comcast.net>
2023-11-05 16:35:48 -0600

    added first draft of theater list query

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,9 +1,26 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
+const addMovie = mapProperties({
+    movie_id: "movies.movie_id",
+    title: "movies.title",
+    runtime_in_minutes: "movies.runtime_in_minutes",
+    rating: "movies.rating",
+    description: "movies.description",
+    image_url: "movies.image_url",
+    created_at: "movies.created_at",
+    updated_at: "movies.updated_at",
+    is_showing: "movies.is_showing",
+    theater_id: "movies.theater_id"
+})
 
 async function list(){
-    return knex("theaters")
-    .select("*");
+    return knex("theaters as t")
+    .join("movies_theaters as mt", {"t.theater_id": "mt.theater_id" })
+    .join("movies as m", {"mt.movie_id": "m.movie_id"})
+    .select("t.*", "m.*", "mt.is_showing","mt.theater_id" )
+    .then(addMovie);
+
 }
 
 module.exports = { list, }
